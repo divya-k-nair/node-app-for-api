b3205aeadfa7bb3db20dfb9eb034ff2dcb8fc9cc
divya-k-nair <divyakaladharannair@gmail.com>
2021-10-20 23:31:26 +0530

    project completed

---
+++ app.js
@@ -0,0 +1,22 @@
+var express=require('express')
+var bodyParser = require('body-parser')
+var app=express()
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+
+app.use(bodyParser.json())
+app.post('/read',(req,res)=>{
+    var getName=req.body.name
+    var getRoll=req.body.rollno
+    res.send(getName+" "+getRoll)
+})
+app.post('/add',(req,res)=>{
+    var getNum1=parseFloat(req.body.num1)
+    var getNum2=parseFloat(req.body.num2)
+    var result=getNum1+getNum2
+    res.json({"result":result})
+})
+
+app.listen(3000,()=>{
+    console.log("server started at http://localhost:3000")
+})
